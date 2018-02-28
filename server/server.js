0ead80da54a3a05d83e372647902b36df272c936
Chao Fan <fanchao05@gmail.com>
2018-02-27 20:49:18 -0500

    setup config

--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@ const express = require('express');
 
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static(publicPath));
 
 
-app.listen(3000, () => {
-  console.log(`Started on port 3000`);
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
