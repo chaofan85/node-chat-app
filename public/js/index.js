d0dc51d8cd5ccdafcf572952f0b43a7ff457617d
Chao Fan <fanchao05@gmail.com>
2018-02-28 18:04:13 -0500

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Chao',
-    text: 'Yup, that works for me'
-  });
 });
 
 socket.on('disconnect', function () {
