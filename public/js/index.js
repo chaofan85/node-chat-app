f83fad13f77bfdb6d46d30650ab5ed913524f36c
Chao Fan <fanchao05@gmail.com>
2018-02-28 17:58:25 -0500

    added newMessage event

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,17 @@ const socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Chao',
+    text: 'Yup, that works for me'
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newEmail', function(email) {
-  console.log('New Email', email);
+socket.on('newMessage', function(message) {
+  console.log('newMessage', message);
 });
