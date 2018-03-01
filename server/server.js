b03bcb0425887de5a9526797180788e27b8ca38e
Chao Fan <fanchao05@gmail.com>
2018-02-28 23:03:07 -0500

    added call back funtion

--- server/server.js
+++ server/server.js
@@ -24,12 +24,14 @@ io.on('connection', (socket) => {
     'Admin', 'New user joined'
   ));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
 
     io.emit('newMessage', generateMessage(
       message.from, message.text
     ));
+
+    callback('This is from the server.');
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
