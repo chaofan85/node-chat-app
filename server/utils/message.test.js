0a4fbf4d8c2d907971b2e56a851ec722a8813679
Chao Fan <fanchao05@gmail.com>
2018-03-01 16:32:54 -0500

    added test case

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -11,5 +11,17 @@ describe('generateMessage', () => {
     expect(result['from']).toBe('Chao');
     expect(result['text']).toBe('I love Randy and Darryl');
     expect(typeof result['createdAt']).toBe('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const author = 'Admin';
+    const latitude = 1;
+    const longitude = 1;
+    const message = generateLocationMessage(author, latitude, longitude);
+
+    expect(message.from).toBe('Admin');
+    expect(message.url).toBe('https://www.google.com/maps?q=1,1');
   });
 });
