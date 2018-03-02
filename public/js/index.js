1624384bc55e78a59f735479f27454df9ae38966
Chao Fan <fanchao05@gmail.com>
2018-03-02 12:33:55 -0500

    added autoscroll feature

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 const socket = io();
 
+function scrollToBottom() {
+  const messages = $('#messages');
+  const newMessage = messages.children('li:last-child');
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -19,7 +33,7 @@ socket.on('newMessage', function(message) {
   });
 
   $('#messages').append(html);
-
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -32,6 +46,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 $('#message-form').on('submit', function(e) {
