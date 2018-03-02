d580f9796d82697c15eb8bee97ced3fec59a4945
Chao Fan <fanchao05@gmail.com>
2018-03-01 19:22:27 -0500

    added moment lib

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+const moment = require('moment');
+//
+// const date = new Date();
+// console.log(date.getMonth());
+
+const date = moment();
+
+const someTimestamp = moment().valueOf();
+
+console.log(someTimestamp);
