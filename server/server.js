7d1c77da5c32c941317433c4ef876e192d9f7e65
Chao Fan <fanchao05@gmail.com>
2018-03-03 19:18:47 -0500

    send messages to the people in the same room

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ io.on('connection', (socket) => {
       'Admin', 'Welcome to the chat app'
     ));
 
-    socket.broadcast.to(params.room).emit('newMessage', generateMessage(
+    socket.to(params.room).emit('newMessage', generateMessage(
       'Admin', `${params.name} has joined.`
     ));
 
@@ -43,24 +43,31 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
+    const user = users.getUser(socket.id);
 
-    io.emit('newMessage', generateMessage(
-      message.from, message.text
-    ));
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(
+        user.name,
+        message.text
+      ));
+    }
 
     callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit(
-      'newLocationMessage',
-      generateLocationMessage(
-        'Admin',
-        coords.latitude,
-        coords.longitude
-      )
-    );
+    const user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'newLocationMessage',
+        generateLocationMessage(
+          user.name,
+          coords.latitude,
+          coords.longitude
+        )
+      );
+    }
   });
 
   socket.on('disconnect', () => {
